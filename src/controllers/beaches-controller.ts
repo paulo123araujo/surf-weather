556e0007f42157e6b59c756058272ebdcbba5a39
Paulo Araujo <paulofelipe_jau7654@hotmail.com>
2022-02-28 22:10:38 -0300

    deal with validation error on create beach

--- src/controllers/beaches-controller.ts
+++ src/controllers/beaches-controller.ts
@@ -1,14 +1,23 @@
 import { Controller, Post } from '@overnightjs/core';
 import { Request, Response } from 'express';
 import { Beach } from '@src/models/beach';
+import mongoose from 'mongoose';
 
 @Controller('beaches')
 export class BeachesController {
   @Post('')
   public async create(req: Request, res: Response): Promise<void> {
-    const beach = new Beach(req.body);
-    const result = await beach.save();
+    try {
+      const beach = new Beach(req.body);
+      const result = await beach.save();
 
-    res.status(201).json(result);
+      res.status(201).json(result);
+    } catch (err) {
+      if (err instanceof mongoose.Error.ValidationError) {
+        res.status(422).json({ error: err.message });
+      } else {
+        res.status(500).send({ error: 'Internal Server Error' });
+      }
+    }
   }
 }
