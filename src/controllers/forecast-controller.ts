7329f6c68a05892e38ead6e2ab3ff2b7bb784aa3
Paulo Araujo <paulofelipe_jau7654@hotmail.com>
2022-03-01 22:24:18 -0300

    call service on forecast controller

--- src/controllers/forecast-controller.ts
+++ src/controllers/forecast-controller.ts
@@ -1,51 +1,17 @@
 import { Controller, Get } from '@overnightjs/core';
+import { Beach } from '@src/models/beach';
+import { ForecastService } from '@src/services/forecast-service';
 import { Request, Response } from 'express';
-
 @Controller('forecast')
 export class ForecastController {
+  constructor(
+    private readonly service: ForecastService = new ForecastService()
+  ) { }
+
   @Get('')
-  public getForecastForLoggedUser(_: Request, res: Response): void {
-    res.status(200).json([
-      {
-        time: '2020-04-26T00:00:00+00:00',
-        forecast: [
-          {
-            lat: -33.792726,
-            lng: 151.289824,
-            name: 'Manly',
-            position: 'E',
-            rating: 2,
-            swellDirection: 64.26,
-            swellHeight: 0.15,
-            swellPeriod: 3.89,
-            time: '2020-04-26T00:00:00+00:00',
-            waveDirection: 231.38,
-            waveHeight: 0.47,
-            windDirection: 299.45,
-            windSpeed: 100,
-          },
-        ],
-      },
-      {
-        time: '2020-04-26T01:00:00+00:00',
-        forecast: [
-          {
-            lat: -33.792726,
-            lng: 151.289824,
-            name: 'Manly',
-            position: 'E',
-            rating: 2,
-            swellDirection: 123.41,
-            swellHeight: 0.21,
-            swellPeriod: 3.67,
-            time: '2020-04-26T01:00:00+00:00',
-            waveDirection: 232.12,
-            waveHeight: 0.46,
-            windDirection: 310.48,
-            windSpeed: 100,
-          },
-        ],
-      },
-    ]);
+  public async  getForecastForLoggedUser(_: Request, res: Response): Promise<void> {
+    const beaches = await Beach.find({});
+    const forecastData = await this.service.processForecastForBeaches(beaches);
+    res.status(200).json(forecastData);
   }
 }
